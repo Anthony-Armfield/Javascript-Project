9c8e66bde56f0aa9411232847d88bb728db9b0e1
Anthony Armfield <anthony.armfield@gmail.com>
2017-07-18 21:10:06 -0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,119 @@
+var resumeData = {
+	summary: {
+		background: 'http://cdn.pcwallart.com/images/light-blue-background-wallpaper-1.jpg',
+		profile: '/media/images/profile-rome.jpg',
+		name: 'Anthony Armfield',
+		title: 'Front-End Web Developer',
+		location: 'Boise, ID',
+		phone: '208-539-7916',
+		email: 'anthony.armfield@gmail.com'
+		},
+
+	skills: ['HTML', 'Python', 'CSS', 'Windows', 'Linux', 'Mac OS', 'Software Testing', 'Project Management', 'Updates Management'],
+
+	experience: [
+		{
+		title: 'Front End Developer/Client Updates Manager',
+		organization: 'Thrive Web Designs',
+		startYear: 2017,
+		endYear: 2017,
+		descriptions: [
+			'Using HTML, CSS, and PHP, commit website updates as per client requests, daily.',
+			'Quarterly WordPress security, core, and plugin updates.'
+			]
+		},
+		{
+		title: 'Founder/Developer',
+		organization: 'Northwest Integrated Concepts',
+		startYear: 2016,
+		endYear: 2017,
+		descriptions: [
+			'Meet with clients to determine project needs.',
+			'Research and develop custom web solutions for clients.',
+			'Create and implement client SEO strategies.',
+			'Conduct social media marketing campaigns and report on results.'
+			]
+		},
+		{
+		title: 'Website Manager',
+		organization: 'Leisureland RV Center',
+		startYear: 2015,
+		endYear: 2017,
+		descriptions: [
+			'Implement new updates to website on a daily basis.',
+			'Front end development using HTML, CSS, PHP, and Javascript.',
+			'Interview and implement SEO changes from third-party recommendations.',
+			'Commit daily backups of hosting server.'
+			]
+		},
+		{
+		title: 'Software Solutions Engineer',
+		organization: 'Quant Algorithms, LLC',
+		startYear: 2014,
+		endYear: 2015,
+		descriptions: [
+			'Scheduling and maintenance of trading algorithm software installations using Tradestation software.',
+			'Managed technical support of trading software after installation with customer interaction through phone and email support.',
+			'Created technical support documentation and videos of trading software and settings.',
+			]
+		},
+		{
+		title: 'Senior Test Engineer II',
+		organization: 'BeyondSoft Consulting inc.',
+		startYear: 2013,
+		endYear: 2014,
+		descriptions: [
+			'Lead team of three Test Engineers through multiple iterations of waterfall method testing HP Printer Firmware and Software',
+			'Technical writing and documentation (i.e. test plans, use cases, matrices, procedural steps, instructional “how to...” reference/training materials, etc…)',
+			'Ability to effectively communicate status, issues, and concerns with all project stakeholders',
+			'Mentoring other team members in the key aspects of technical or functional processes and quality assurance best practices',
+			'Performed manual testing to ensure the software meets stated requirements & designs, and has a positive user experience',
+			'Planned and executed black box testing using defined methodologies for manual tests',
+			'Worked closely with developers and program managers on defect resolution',
+			'Highly analytic approach to executing tasks with ability to multi-task and work effectively in a fast-paced environment'
+			]
+		},
+		{
+		title: 'Junior Software Test Engineer/Test Lead',
+		organization: 'Lionbridge Technologies',
+		startYear: 2010,
+		endYear: 2013,
+		descriptions: [
+			'Lead a team of four testers in functionality and localization testing of Android OS on new mobile device; team was awarded contract extension due to diligent reporting of defects in a timely manner',
+			'2+ years of Software Quality Assurance experience including a demonstrated ability to define test strategies, create test documentation, systematically identify and isolate defects, meticulously report defects, and validate fixes',
+			'Technical writing and documentation (i.e. test plans, use cases, matrices, procedural steps, instructional “how to...” reference/training materials, etc…)',
+			'Mentoring other team members in the key aspects of technical or functional processes and quality assurance best practices',
+			'Fully competent in the design, implementation, and maintenance of a test environment, including necessary equipment, configuration, and software',
+			'Demonstrated leadership in driving the implementation of QA solutions and tools',
+			'Assist in the creation and documentation of test requirements and high level test cases, followed by creation and execution of detailed test cases',
+			'Worked closely with developers and program managers on defect resolution',
+			'Highly analytic approach to executing tasks with ability to multi-task and work effectively in a fast-paced environment'
+			]
+		}
+	],
+	education: [
+		{
+		title: 'Computer Network Systems',
+		organization: 'ITT Technical Institute',
+		startYear: 2008,
+		endYear: 2010,
+		descriptions: [
+			'Server Technologies',
+			'Linux Operating Systems',
+			'Intro to Computer Programming'
+			]
+		},
+		{
+		title: 'General Diploma',
+		organization: 'Timberline High School',
+		startYear: 1999,
+		endYear: 2003,
+		descriptions: [
+			'Zoology',
+			'Practical Entomology',
+			'Latin',
+			'Jr. ROTC'
+			]
+		}
+	]			
+}
